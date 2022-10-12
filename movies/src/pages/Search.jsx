a5a1f07a7fbe65a02d453da65deae10916e72622
Juliana Mendes <juliana-dantas@live.com>
2022-10-11 21:00:39 -0300

    first commit

---
+++ movies/src/pages/Search.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react"
+import { useSearchParams } from "react-router-dom"
+import MovieCard from "../components/MovieCard"
+
+const searchURL = import.meta.env.VITE_SEARCH
+const apiKey = import.meta.env.VITE_API_KEY
+
+import './MoviesGrid.css'
+
+const Search = () => {
+  const [searchParams] = useSearchParams()
+
+  const [movies, setMovies] = useState([])
+  const query = searchParams.get("q")
+
+  const getSearchMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setMovies(data.results)
+  }
+
+  useEffect(() => {
+    const searchWithQueryUrl = `${searchURL}?${apiKey}&query=${query}`
+   
+    getSearchMovies(searchWithQueryUrl )
+  }, [query])
+
+  return (
+    <div className="container">
+    <h2 className="title"> Resultados para: <span className="query-text"> {query}</span></h2>
+    <div className="movies-container">
+    {movies.length > 0 &&
+          movies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
+    </div>
+     </div>
+  )
+}
+
+export default Search
